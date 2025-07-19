f3a4df46daf35dc75efe825ca85e396b673fa299
cendress <95492688+cendress@users.noreply.github.com>
2025-07-19 10:37:34 -0400

    Create Currency data model

---
+++ Countries/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  Countries
+//
+//  Created by Christopher Endress on 7/19/25.
+//
+
+import Foundation
+
+struct Currency: Codable {
+    let code: String
+    let name: String
+    let symbol: String
+}
