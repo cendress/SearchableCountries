02426d347d77921870caa3124b7e14c449b44ed9
cendress <95492688+cendress@users.noreply.github.com>
2025-07-19 14:30:28 -0400

    Enable debouncing

--- Countries/Views/CountryListViewController.swift
+++ Countries/Views/CountryListViewController.swift
@@ -13,29 +13,32 @@ final class CountryListViewController: UIViewController {
     private let searchController = UISearchController(searchResultsController: nil)
     private let viewModel = CountryListViewModel()
     private var cancellables = Set<AnyCancellable>()
-    
+    private let searchTextSubject = PassthroughSubject<String, Never>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Enable large titles
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
         title = "Countries"
         view.backgroundColor = .systemBackground
+
         setupTableView()
         setupSearchController()
         bindViewModel()
     }
-    
-    // MARK: - Setup
-    
+
+    // MARK: - UI setup
+
     private func setupTableView() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(CountryCell.self, forCellReuseIdentifier: CountryCell.reuseID)
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.estimatedRowHeight = 60
         tableView.rowHeight = UITableView.automaticDimension
-        
+
         view.addSubview(tableView)
-        
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -43,23 +46,28 @@ final class CountryListViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-    
+
     private func setupSearchController() {
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search by name or capital"
         navigationItem.searchController = searchController
         definesPresentationContext = true
-        
-        // Bind search bar text to the view model
-        searchController.searchBar
-            .publisher(for: \.text)
-            .compactMap { $0 }
-            .assign(to: \.searchText, on: viewModel)
+
+        // Use delegate to catch text changes
+        searchController.searchBar.delegate = self
+
+        // Debounce text input from delegate
+        searchTextSubject
+            .debounce(for: .milliseconds(300), scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .sink { [weak self] text in
+                self?.viewModel.searchText = text
+            }
             .store(in: &cancellables)
     }
-    
+
     // MARK: - Binding
-    
+
     private func bindViewModel() {
         viewModel.$filtered
             .receive(on: DispatchQueue.main)
@@ -67,7 +75,7 @@ final class CountryListViewController: UIViewController {
                 self?.tableView.reloadData()
             }
             .store(in: &cancellables)
-        
+
         viewModel.$errorMessage
             .compactMap { $0 }
             .sink { [weak self] message in
@@ -75,7 +83,7 @@ final class CountryListViewController: UIViewController {
             }
             .store(in: &cancellables)
     }
-    
+
     private func presentAlert(title: String, message: String) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -83,19 +91,34 @@ final class CountryListViewController: UIViewController {
     }
 }
 
-// MARK: - UITableView data source
+// MARK: - UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate
 
-extension CountryListViewController: UITableViewDataSource {
+extension CountryListViewController: UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel.filtered.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryCell.reuseID, for: indexPath) as? CountryCell
-        else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CountryCell.reuseID, for: indexPath) as? CountryCell else {
+            return UITableViewCell()
+        }
         
         let country = viewModel.filtered[indexPath.row]
         cell.configure(with: country)
         return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    // Receive live text changes for debounce
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchTextSubject.send(searchText)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        viewModel.searchText = searchBar.text ?? ""
+        searchBar.resignFirstResponder()
     }
 }
